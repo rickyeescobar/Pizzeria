1fbe96d6756b1b76e836598ab8fd304692dcbde7
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-04-22 14:55:59 -0500

    added admin

--- pizzas/models.py
+++ pizzas/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Pizza(models.Model):
-    name = model.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    date_added = models.DateTimeField(auto_created=True)
 
     def __str__(self):
         return self.name
+
+#class Topping(models.Model):
+    #pizza = Topping
