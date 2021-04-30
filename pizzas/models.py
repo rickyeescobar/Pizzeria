2bb19e466fce566719b5b401d561545f1eae696a
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-04-29 22:05:13 -0500

    worked

--- pizzas/models.py
+++ pizzas/models.py
@@ -5,10 +5,13 @@ from django.db import models
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
 
-    date_added = models.DateTimeField(auto_created=True)
 
     def __str__(self):
         return self.name
 
-#class Topping(models.Model):
-    #pizza = Topping
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Pizza, Topping 
 
 # Create your views here.
+
+def index(request):
+    """The home page for pizzas"""
+    return render(request, 'pizzas/index.html')
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('name')
+    #filter(name=request)
+
+    context = {'pizzas':pizzas}
+
+    return render(request, 'pizzas/pizzas.html', context)
+
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    toppings = pizza.topping_set.order_by('name')
+
+    context = {'pizza':pizza, 'toppings':toppings}
+
+    return render(request, 'pizzas/pizza.html', context)
