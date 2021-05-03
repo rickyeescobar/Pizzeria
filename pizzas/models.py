2c2970ddf91a15ee8230195cdcd2957f7d867f78
Ricky Escobar <rickyeescobar1999@gmail.com>
2021-05-03 15:42:10 -0500

    got everything working. fixed pictures

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
-
+    
+    header_image = models.ImageField(
+        null=True, blank=True, upload_to='images/')
 
     def __str__(self):
         return self.name
